b8dc301b6c8ccc81d458980cf6398802d3bda3b2
Veronica0711 <veronicalim81@gmail.com>
2025-06-21 08:35:34 +0800

    Update

---
+++ root__lesson6.js
@@ -0,0 +1,42 @@
+// root.js
+
+function WelcomeMessagesTitle() {
+  return <p>Welcome Messages</p>
+}
+
+function WelcomeMessage(props) {
+  return <li>Welcome {props.name} from {props.country}</li>
+}
+
+function WelcomeMessagesList() {
+  return <ul>
+    <WelcomeMessage 
+    name='Tom' 
+    country='Singapore' 
+    />
+    <WelcomeMessage 
+    name='Jerry' 
+    country='Malaysia' 
+    />
+    <WelcomeMessage 
+    name='Sarah' 
+    country='Indonesia' />
+  </ul>
+}
+
+function WelcomeMessagesSection() {
+  return <div>
+    <WelcomeMessagesTitle />
+    <WelcomeMessagesList />
+  </div>
+}
+
+function App() {
+  return <div>
+    <WelcomeMessagesSection />
+  </div>
+}
+
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<App />)
